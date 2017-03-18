5fda88a50198345de39d0ea1cc2f3694a2ef12d4
qiulihua83 <qiulihua83@qq.com>
2017-03-18 23:36:09 +0800

    add2num_linked implement

--- add2numlinked/add_2_num_linked.cpp
+++ add2numlinked/add_2_num_linked.cpp
@@ -10,7 +10,49 @@ list<int>  add2num_linked(list<int> n1, list<int> n2)
 {
     list<int> sum;
 
+    int n = 0;
+    int up = 0;
+    list<int>::iterator it1 = n1.begin();
+    list<int>::iterator it2 = n2.begin();
 
+    while((it1 != n1.end()) && (it2 != n2.end()))
+    {
+        n  = (*it1 + *it2 + up)%10;
+        up = (*it1 + *it2 + up)/10;
+
+        sum.push_back(n);
+
+        it1++;
+        it2++;
+    }
+
+    list<int>::iterator lb;
+    list<int>::iterator le;
+    if(it1 != n1.end())
+    {
+        lb = it1;
+        le = n1.end();
+    }
+    else if(it2 != n2.end())
+    {
+        lb = it2;
+        le = n2.end();
+    }
+    else
+    {
+        lb = le = n1.end();
+    }
+    // insert left
+    for(; lb != le; lb++)
+    {
+        n  = (*lb + up)%10;
+        up = (*lb + up)/10;
+        sum.push_back(n);
+    }
+    if(0 != up)
+    {
+        sum.push_back(up);
+    }
     return sum;
 }
 
@@ -23,23 +65,30 @@ bool test_add2num_linked()
     list<int> n2;
     list<int> sum;
 
-    n1.push_front(2);
+    n1.push_front(9);
+    n1.push_front(8);
     n1.push_front(4);
-    n1.push_front(3);
+    n1.push_front(2);
 
-    n2.push_front(5);
-    n2.push_front(6);
     n2.push_front(4);
+    n2.push_front(6);
+    n2.push_front(5);
 
-    sum.push_front(7);
+    sum.push_front(1);
     sum.push_front(0);
-    sum.push_front(8);
+    sum.push_front(3);
+    sum.push_front(0);
+    sum.push_front(7);
 
     list<int> add = add2num_linked(n1, n2);
 
     ret = equal(sum.begin(), sum.end(), add.begin());
 
     cout << "\n add2num_linked() result : " << ret  << endl;
+    for(list<int>::iterator it=add.begin(); it != add.end(); it++)
+    {
+        cout << *it << " <- " ;
+    }
 
     return ret;
 }
