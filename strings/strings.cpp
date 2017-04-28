5be260f32641a48c00d48f28017e3e4f7278c70c
andychiu <qiulihua83@qq.com>
2017-04-28 22:47:09 +0800

    add strings

---
+++ strings/strings.cpp
@@ -0,0 +1,26 @@
+//
+// Created by QiuLihua on 2017/4/28.
+//
+
+#include "strings.h"
+
+#include "../common/comm.h"
+
+
+/*字符串中第一个只出现一次的字符*/
+char first_char_appeared_1s(char * pch)
+{
+    char c = '0';
+
+
+    return c;
+}
+
+
+bool test_first_char_appeared_1s()
+{
+    bool bret = false;
+
+
+    return bret;
+}
---
+++ strings/strings.h
@@ -0,0 +1,11 @@
+//
+// Created by QiuLihua on 2017/4/28.
+//
+
+#ifndef ALGORITHM_STRINGS_H
+#define ALGORITHM_STRINGS_H
+
+
+bool test_first_char_appeared_1s();
+
+#endif //ALGORITHM_STRINGS_H
