7aa709f76c016348b14a4379300e87715d6ca4ff
andylhqiu <qiulihua83@qq.com>
2026-01-02 15:23:57 +0800

    add build foulder

---
+++ build/default/CMakeFiles/algorithm.dir/DependInfo.cmake
@@ -0,0 +1,42 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/qiulihua/git/my/algorithm/SimpleBinarySearchTree/SimpleBinarySearchTree.cpp" "CMakeFiles/algorithm.dir/SimpleBinarySearchTree/SimpleBinarySearchTree.cpp.o" "gcc" "CMakeFiles/algorithm.dir/SimpleBinarySearchTree/SimpleBinarySearchTree.cpp.o.d"
+  "/Users/qiulihua/git/my/algorithm/add2numlinked/add_2_num_linked.cpp" "CMakeFiles/algorithm.dir/add2numlinked/add_2_num_linked.cpp.o" "gcc" "CMakeFiles/algorithm.dir/add2numlinked/add_2_num_linked.cpp.o.d"
+  "/Users/qiulihua/git/my/algorithm/array/array_op.cpp" "CMakeFiles/algorithm.dir/array/array_op.cpp.o" "gcc" "CMakeFiles/algorithm.dir/array/array_op.cpp.o.d"
+  "/Users/qiulihua/git/my/algorithm/array/test_array.cpp" "CMakeFiles/algorithm.dir/array/test_array.cpp.o" "gcc" "CMakeFiles/algorithm.dir/array/test_array.cpp.o.d"
+  "/Users/qiulihua/git/my/algorithm/ballthrow/ballthrow.cpp" "CMakeFiles/algorithm.dir/ballthrow/ballthrow.cpp.o" "gcc" "CMakeFiles/algorithm.dir/ballthrow/ballthrow.cpp.o.d"
+  "/Users/qiulihua/git/my/algorithm/binary_number/binary_number.cpp" "CMakeFiles/algorithm.dir/binary_number/binary_number.cpp.o" "gcc" "CMakeFiles/algorithm.dir/binary_number/binary_number.cpp.o.d"
+  "/Users/qiulihua/git/my/algorithm/lib/array.cpp" "CMakeFiles/algorithm.dir/lib/array.cpp.o" "gcc" "CMakeFiles/algorithm.dir/lib/array.cpp.o.d"
+  "/Users/qiulihua/git/my/algorithm/lib/binary_tree.cpp" "CMakeFiles/algorithm.dir/lib/binary_tree.cpp.o" "gcc" "CMakeFiles/algorithm.dir/lib/binary_tree.cpp.o.d"
+  "/Users/qiulihua/git/my/algorithm/lib/linked_list.cpp" "CMakeFiles/algorithm.dir/lib/linked_list.cpp.o" "gcc" "CMakeFiles/algorithm.dir/lib/linked_list.cpp.o.d"
+  "/Users/qiulihua/git/my/algorithm/linked_list/linked_list_op.cpp" "CMakeFiles/algorithm.dir/linked_list/linked_list_op.cpp.o" "gcc" "CMakeFiles/algorithm.dir/linked_list/linked_list_op.cpp.o.d"
+  "/Users/qiulihua/git/my/algorithm/linked_list/test_linked_list.cpp" "CMakeFiles/algorithm.dir/linked_list/test_linked_list.cpp.o" "gcc" "CMakeFiles/algorithm.dir/linked_list/test_linked_list.cpp.o.d"
+  "/Users/qiulihua/git/my/algorithm/main.cpp" "CMakeFiles/algorithm.dir/main.cpp.o" "gcc" "CMakeFiles/algorithm.dir/main.cpp.o.d"
+  "/Users/qiulihua/git/my/algorithm/reverse_btree/reverse_btree.cpp" "CMakeFiles/algorithm.dir/reverse_btree/reverse_btree.cpp.o" "gcc" "CMakeFiles/algorithm.dir/reverse_btree/reverse_btree.cpp.o.d"
+  "/Users/qiulihua/git/my/algorithm/reverse_linkedlist/reverse_linkedlist.cpp" "CMakeFiles/algorithm.dir/reverse_linkedlist/reverse_linkedlist.cpp.o" "gcc" "CMakeFiles/algorithm.dir/reverse_linkedlist/reverse_linkedlist.cpp.o.d"
+  "/Users/qiulihua/git/my/algorithm/search/search.cpp" "CMakeFiles/algorithm.dir/search/search.cpp.o" "gcc" "CMakeFiles/algorithm.dir/search/search.cpp.o.d"
+  "/Users/qiulihua/git/my/algorithm/search/test_search.cpp" "CMakeFiles/algorithm.dir/search/test_search.cpp.o" "gcc" "CMakeFiles/algorithm.dir/search/test_search.cpp.o.d"
+  "/Users/qiulihua/git/my/algorithm/sort/sort.cpp" "CMakeFiles/algorithm.dir/sort/sort.cpp.o" "gcc" "CMakeFiles/algorithm.dir/sort/sort.cpp.o.d"
+  "/Users/qiulihua/git/my/algorithm/sort/test_sort.cpp" "CMakeFiles/algorithm.dir/sort/test_sort.cpp.o" "gcc" "CMakeFiles/algorithm.dir/sort/test_sort.cpp.o.d"
+  "/Users/qiulihua/git/my/algorithm/strings/strings.cpp" "CMakeFiles/algorithm.dir/strings/strings.cpp.o" "gcc" "CMakeFiles/algorithm.dir/strings/strings.cpp.o.d"
+  "/Users/qiulihua/git/my/algorithm/test.cpp" "CMakeFiles/algorithm.dir/test.cpp.o" "gcc" "CMakeFiles/algorithm.dir/test.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/default/CMakeFiles/algorithm.dir/test.cpp.o.d
@@ -0,0 +1,2 @@
+CMakeFiles/algorithm.dir/test.cpp.o: \
+  /Users/qiulihua/git/my/algorithm/test.cpp
